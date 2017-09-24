eca7ae23bd0a345c278c4ca3e1c9f75772800d46
Mattk205 <mattk205@gmail.com>
2017-09-24 16:53:10 -0400

    checkpoint 27 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,9 +41,17 @@ topics = Topic.all
    )
  end
 
- user = User.first
- user.update_attributes!(
-   email: 'mattk205@gmail.com', # replace this with your personal email
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
 
