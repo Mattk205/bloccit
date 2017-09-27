d7e2a233300b463d00a32ab8a8fef2f9bf7bc9f3
Mattk205 <mattk205@gmail.com>
2017-09-27 14:53:45 -0400

    checkpoint 28 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   before_save { self.email = email.downcase if email.present? }
-   before_save { self.role ||= :member }
+  before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
